f8f2853793a52aa8a1463bae76fc9d2809cc8871
omark807 <omark807@gmail.com>
2022-11-07 16:00:43 -0600

    Added documentation for nav bar elements

--- app/src/main/java/com/illinois/safetyratingsuiuc/ResourcesActivity.java
+++ app/src/main/java/com/illinois/safetyratingsuiuc/ResourcesActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 
 public class ResourcesActivity extends AppCompatActivity {
 
+    // Create attributes for the navigation drawer and toggle button at the top right
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
 
@@ -19,16 +20,22 @@ public class ResourcesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_resources);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        // Two strings required to describe the 'open' and 'close' actions for accessibility
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
 
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
 
+        // Lets the user see the 'back' navigation button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
     }
 
+    /**
+     * Invoked whenever an item in the options menu is selected.
+     * @param item The menu item that is selected -- cannot be null
+     * @return false, if we want to allow menu processing to continue, true otherwise
+     */
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (mToggle.onOptionsItemSelected(item)) {
