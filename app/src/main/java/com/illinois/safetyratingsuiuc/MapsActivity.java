e210b02fd808a5265c8df8e9cab24458d87a47eb
S4MU3LZ <42338321+S4MU3LZ@users.noreply.github.com>
2022-11-08 13:48:11 +0800

    added search bar but having errors

--- app/src/main/java/com/illinois/safetyratingsuiuc/MapsActivity.java
+++ app/src/main/java/com/illinois/safetyratingsuiuc/MapsActivity.java
@@ -1,25 +1,33 @@
 package com.illinois.safetyratingsuiuc;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.RectangularBounds;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.illinois.safetyratingsuiuc.databinding.ActivityMapsBinding;
+
+import java.util.Arrays;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
-
-    // Initialize the AutocompleteSupportFragment.
-    AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
-            getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +40,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        String apiKey = null;
+        try {
+            apiKey = this.getPackageManager().getApplicationInfo(this.getPackageName(), PackageManager.GET_META_DATA).metaData.getString("com.google.android.geo.API_KEY");
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), apiKey);
+        }
+
+
+        // Initialize the AutocompleteSupportFragment.
+
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+
+        autocompleteFragment.setLocationRestriction(RectangularBounds.newInstance(new LatLng(-40.06943400464524, -88.31494084238419), new LatLng(40.158178564972815, -88.16191394471619)));
+
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
     }
 
     /**
@@ -48,27 +93,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng mainQuad = new LatLng(40.1075, -88.2272);
-        mMap.addMarker(new MarkerOptions().position(mainQuad).title("Marker at Main Quad"));
+        LatLng mainQuad = new LatLng(40.10751713396527, -88.22722016817475);
+        mMap.addMarker(new MarkerOptions().position(mainQuad).title("Marker in Main Quad"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(mainQuad));
     }
-
-    // Specify the types of place data to return.
-    autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
-
-    // Set up a PlaceSelectionListener to handle the response.
-    autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-        @Override
-        public void onPlaceSelected(@NonNull Place place) {
-            // TODO: Get info about the selected place.
-            Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
-        }
-
-
-        @Override
-        public void onError(@NonNull Status status) {
-            // TODO: Handle the error.
-            Log.i(TAG, "An error occurred: " + status);
-        }
-    });
 }
